0d44125d3c4e0f2b36b4e478cbe82e0db943fa0b
fulishi <35857551+fulishi@users.noreply.github.com>
2022-09-02 18:05:37 +0800

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -2,18 +2,18 @@
 module.exports = {
   wx: {
     token: "hanbao",
-    appId: "wx3fd1ed9a525a15b3",
-    appsecret: "a1a20a429d7a24e3670ce88c7c47068e",
+    appId: "wx2955f5310e5f4e4b",
+    appsecret: "c814bfb81e619e42c1c286d72fc99231",
     //模板id
-    template_id: "vKv0DLlc69o5Y1INWz1T0kp3t4FtJFac-D4K74gI2q8",
+    template_id: "BQK14R7WUBLaxre5XzY71KY7P-2qJBc4-AisMreyNTk",
   },
   // 要推送的用户openid和信息
   users: {
-    openid: "oiQLt6okPWNC_0aZY4wqUxjP4xPc",
-    city: "福州市", //支持国内城市行政区划代码（建议）和城市中文名称查询（市区县）
+    openid: "oixiO6VJ1kivBmNxP_bK-ZCRiFXc",
+    city: "乐东", //支持国内城市行政区划代码（建议）和城市中文名称查询（市区县）
     // 生日
-    month: "03", //月
-    day: "30", //日
+    month: "05", //月
+    day: "28", //日
   },
 
   // 天行api 的APIKEY
